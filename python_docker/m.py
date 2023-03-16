ddfe0c8be78d482fbd6dc19795bd9a3031cabd4f
PHXA <69100913+PHXA@users.noreply.github.com>
2023-03-17 02:52:56 +0900

    first

---
+++ python_docker/m.py
@@ -0,0 +1,17 @@
+import subprocess
+import time, datetime
+
+DURATION_SEC=10*60
+
+f_p = "/result/speed.tsv"
+with open(f_p,"w") as f:
+  f.write("time\tdownload[Mbit/s]\tupload[Mbit/s]\n")
+
+while 1:
+  s=time.time()
+  a=subprocess.run("speedtest", capture_output=True, text=True).stdout
+  a=a.split()
+  with open(f_p,"a") as f:
+    f.write(f"{datetime.datetime.now()}\t{a[29]}\t{a[35]}\n")
+  e=time.time()
+  time.sleep(max(DURATION_SEC-(e-s),0))
